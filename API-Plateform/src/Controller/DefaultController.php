18f191783168d68296f8bdc11c027b7c964c2c71
Sandra <sandra.villot038@gmail.com>
2022-06-07 16:32:09 +0200

    affichage de l'entity Team

--- API-Plateform/src/Controller/DefaultController.php
+++ API-Plateform/src/Controller/DefaultController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Position;
+use App\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route("/default", name="app_default")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $team = $doctrine->getRepository(Team::class)->findAll();
         return $this->render('default/index.html.twig', [
-            'message' => 'Voici mon API en symfony',
+            'team' => $team
         ]);
     }
 }
